483c8c70fa945e8fbca78e2fb66addeb1cca4c45
Alucherdi <hjgo127942@gmail.com>
2020-01-23 00:05:57 -0600

    Initial Commit

---
+++ webview_delegate_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webview_delegate_test
+//
+//  Created by Alucherdi on 23/01/20.
+//  Copyright © 2020 Alucherdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
