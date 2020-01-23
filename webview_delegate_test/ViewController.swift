ac6b48b511979fe7d49e39a00fa7c5ce54bbfdc8
Alucherdi <hjgo127942@gmail.com>
2020-01-23 00:27:13 -0600

    Init commit

--- webview_delegate_test/ViewController.swift
+++ webview_delegate_test/ViewController.swift
@@ -7,9 +7,21 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKUIDelegate {
+    var webView : WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.uiDelegate = self
+        view = webView
+        
+        let url = URL(string: "https://public.undervolt.io/gyro.html")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
